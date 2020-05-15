9dccbe9f58d466980d3533cbf012d639179c020c
Saul Bento <saulbento@gmail.com>
2020-05-15 19:35:05 +0200

    update

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -44,8 +44,6 @@ const SignIn: React.FC = () => {
 
   const { signIn, user } = useAuth();
 
-  console.log(user);
-
   const handleSignIn = useCallback(async (data: SignInFormData) => {
     try {
       formRef.current?.setErrors({});
